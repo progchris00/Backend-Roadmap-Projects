58ad4cee68ca4c2a6fe9c36832276255ef10d51e
progchris00 <prog.christian07@gmail.com>
2024-08-24 13:48:51 +0800

    feat: add name value on select elements to use in result.php

--- main.php
+++ main.php
@@ -14,7 +14,7 @@
         <label for="length">Enter the length to convert:</label>
         <input name="length" id="length" type="number">
         <label for="convert_unit_from">Unit to Convert from</label>
-        <select name="" id="">
+        <select name="convert_unit_from" id="">
             <option value="kilometer">kilometer</option>
             <option value="meter">meter</option>
             <option value="centimeter">centimeter</option>
@@ -22,7 +22,7 @@
             <option value="foot">foot</option>
         </select>
         <label for="convert_unit_to">Unit to Convert to</label>
-        <select name="" id="">
+        <select name="convert_unit_to" id="">
             <option value="kilometer">kilometer</option>
             <option value="meter">meter</option>
             <option value="centimeter">centimeter</option>
