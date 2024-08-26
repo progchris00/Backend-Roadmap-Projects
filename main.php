48309459c9651e6ab6e8e43d91297b6f861db3eb
progchris00 <prog.christian07@gmail.com>
2024-08-26 13:40:14 +0800

    feat: finish markup

--- main.php
+++ main.php
@@ -4,33 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Unit Converter</title>
 </head>
 
 <body>
-    <form action="result.php" method="post">
+    <main>
         <h1>Unit Converter</h1>
-        <label for="length">Enter the length to convert:</label>
-        <input name="length" id="length" type="number">
-        <label for="convert_unit_from">Unit to Convert from</label>
-        <select name="convert_unit_from" id="">
-            <option value="kilometer">kilometer</option>
-            <option value="meter">meter</option>
-            <option value="centimeter">centimeter</option>
-            <option value="millimeter">millimeter</option>
-            <option value="foot">foot</option>
-        </select>
-        <label for="convert_unit_to">Unit to Convert to</label>
-        <select name="convert_unit_to" id="">
-            <option value="kilometer">kilometer</option>
-            <option value="meter">meter</option>
-            <option value="centimeter">centimeter</option>
-            <option value="millimeter">millimeter</option>
-            <option value="foot">foot</option>
-        </select>
-        <button type="submit">Submit</button>
-    </form>
+        <div class="choices">
+            <p>Length</p>
+            <p>Weight</p>
+            <p>Temperature</p>
+        </div>
+        <form action="result.php" method="post">
+            <label for="length">Enter the length to convert:</label>
+            <input name="length" id="length" type="number">
+            <label for="convert_unit_from">Unit to Convert from</label>
+            <select name="convert_unit_from" id="">
+            </select>
+            <label for="convert_unit_to">Unit to Convert to</label>
+            <select name="convert_unit_to" id="">
+            </select>
+            <button type="submit">Convert</button>
+        </form>
+    </main>
 </body>
 
 </html>
