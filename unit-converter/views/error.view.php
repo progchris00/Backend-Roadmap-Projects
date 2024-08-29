b0ce740f64db823108fcd08283eac5eaf95d891a
progchris00 <prog.christian07@gmail.com>
2024-08-29 21:40:02 +0800

    feat: update error view

---
+++ unit-converter/views/error.view.php
@@ -0,0 +1,4 @@
+<?php
+require "partials/head.php";
+require "partials/error-message.php";
+require "partials/foot.php";
---
+++ unit-converter/views/partials/error-message.php
@@ -0,0 +1,1 @@
+<p class="error-message">Page not found. Go back <a href="/"> home </a> </p>
