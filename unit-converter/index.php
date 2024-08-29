638e14c1da06509d09189daeb8a88d46265c08be
progchris00 <prog.christian07@gmail.com>
2024-08-29 19:42:09 +0800

    feat: change conditional statements to associative arrays

--- unit-converter/index.php
+++ unit-converter/index.php
@@ -1,13 +1,14 @@
 <?php
 
+
 require "functions.php";
+
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($uri === "/" || $uri === "/home") {
-    require "controllers/home.php";
-} else if ($uri === "/result") {
-    require "controllers/result.php";
-} else {
-    require "controllers/error.php";
-}
+
+$routes = [
+    "/" => "controllers/home.php",
+    "/home" => "controllers/home.php",
+    "/result" => "controllers/result.php",
+];
