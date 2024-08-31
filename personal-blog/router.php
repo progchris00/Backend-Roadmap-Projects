dde8c18c9a5ed1a78ea1d0a5fcd8929b66c7e2ff
progchris00 <prog.christian07@gmail.com>
2024-08-31 15:05:56 +0800

    feat: add route for register

--- personal-blog/router.php
+++ personal-blog/router.php
@@ -5,6 +5,7 @@ $uri = $_SERVER["REQUEST_URI"];
 $routes = [
     "/" => "controllers/home.php",
     "/login" => "controllers/login.php",
+    "/register" => "controllers/register.php",
 ];
 
 if (array_key_exists($uri, $routes)) {
---
+++ personal-blog/views/register.view.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$title = "Login";
+
+
+require "views/partials/head.php";
+require "views/partials/register-form.php";
