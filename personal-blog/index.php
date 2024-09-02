2a30043c513c8dab57f77e9c927aa8d15e982484
progchris00 <prog.christian07@gmail.com>
2024-09-02 20:42:20 +0800

    feat: testing database on blogs

--- personal-blog/index.php
+++ personal-blog/index.php
@@ -2,8 +2,3 @@
 
 require "database.config.php";
 require "router.php";
-
-$pdo = new PDO("mysql:host=localhost;dbname=test_db", DATABASE_USERNAME, DATABASE_PASSWORD);
-$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-
-$unbufferedResult = $pdo->query("SELECT * FROM users");
--- personal-blog/views/partials/blog-contents.php
+++ personal-blog/views/partials/blog-contents.php
@@ -1,87 +1,42 @@
 <div class="">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            <article class="flex max-w-xl flex-col items-start justify-between">
-                <div class="flex items-center gap-x-4 text-xs">
-                    <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                    <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-                </div>
-                <div class="group relative">
-                    <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Boost your conversion rate
-                        </a>
-                    </h3>
-                    <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                </div>
-                <div class="relative mt-8 flex items-center gap-x-4">
-                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-                    <div class="text-sm leading-6">
-                        <p class="font-semibold text-gray-900">
+            <?php
+            $pdo = new PDO("mysql:host=localhost;dbname=test_db", DATABASE_USERNAME, DATABASE_PASSWORD);
+            $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+
+            $unbufferedResult = $pdo->query("SELECT * FROM blogs WHERE isPublic = 1");
+
+            foreach ($unbufferedResult as $blog) { ?>
+
+                <article class="flex max-w-xl flex-col items-start justify-between">
+                    <div class="flex items-center gap-x-4 text-xs">
+                        <time datetime="2020-03-16" class="text-gray-500"><?= $blog['date'] ?></time>
+                        <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $blog['category'] ?></a>
+                    </div>
+                    <div class="group relative">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                             <a href="#">
                                 <span class="absolute inset-0"></span>
-                                Michael Foster
+                                <?= $blog['title'] ?>
                             </a>
-                        </p>
-                        <p class="text-gray-600">Co-Founder / CTO</p>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= $blog['body'] ?></p>
                     </div>
-                </div>
-            </article>
-            <article class="flex max-w-xl flex-col items-start justify-between">
-                <div class="flex items-center gap-x-4 text-xs">
-                    <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                    <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-                </div>
-                <div class="group relative">
-                    <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Boost your conversion rate
-                        </a>
-                    </h3>
-                    <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                </div>
-                <div class="relative mt-8 flex items-center gap-x-4">
-                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-                    <div class="text-sm leading-6">
-                        <p class="font-semibold text-gray-900">
-                            <a href="#">
-                                <span class="absolute inset-0"></span>
-                                Michael Foster
-                            </a>
-                        </p>
-                        <p class="text-gray-600">Co-Founder / CTO</p>
+                    <div class="relative mt-8 flex items-center gap-x-4">
+                        <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                        <div class="text-sm leading-6">
+                            <p class="font-semibold text-gray-900">
+                                <a href="#">
+                                    <span class="absolute inset-0"></span>
+                                    Michael Foster
+                                </a>
+                            </p>
+                            <p class="text-gray-600">Co-Founder / CTO</p>
+                        </div>
                     </div>
-                </div>
-            </article>
-            <article class="flex max-w-xl flex-col items-start justify-between">
-                <div class="flex items-center gap-x-4 text-xs">
-                    <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
-                    <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a>
-                </div>
-                <div class="group relative">
-                    <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Boost your conversion rate
-                        </a>
-                    </h3>
-                    <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                </div>
-                <div class="relative mt-8 flex items-center gap-x-4">
-                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
-                    <div class="text-sm leading-6">
-                        <p class="font-semibold text-gray-900">
-                            <a href="#">
-                                <span class="absolute inset-0"></span>
-                                Michael Foster
-                            </a>
-                        </p>
-                        <p class="text-gray-600">Co-Founder / CTO</p>
-                    </div>
-                </div>
-            </article>
+                </article>
+            <?php } ?>
         </div>
     </div>
 </div>
