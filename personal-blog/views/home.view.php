684cdd2582280aae5ad4ff3508a2b84bf87e7722
progchris00 <prog.christian07@gmail.com>
2024-09-03 18:16:11 +0800

    feat: instantiate and query blogs in home.view
    This will give access to the blogs-content partial the variable public blogs and allow it to render blogs in it

--- personal-blog/views/home.view.php
+++ personal-blog/views/home.view.php
@@ -3,6 +3,8 @@
 
 $title = "Blogs";
 
+$db = new Database();
+$public_blogs = $db->query("SELECT * FROM blogs WHERE isPublic = 1");
 
 require "views/partials/head.php";
 require "views/partials/nav.php";
