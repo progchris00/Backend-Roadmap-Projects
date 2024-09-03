102b2cf8a9c33dd7f4856a3c8677aa63bff355fe
progchris00 <prog.christian07@gmail.com>
2024-09-03 19:40:00 +0800

    feat: create a constructor function
    This will prevent the repetition of connection everytime we query the database

--- personal-blog/DATABASE.php
+++ personal-blog/DATABASE.php
@@ -5,15 +5,20 @@ require "database.config.php";
 
 class Database
 {
-    public function query($query)
+    public $connection;
+
+
+    public function __construct()
     {
         $dsn = "mysql:host=localhost;dbname=test_db;{DATABASE_USERNAME}";
 
-        $database_handle = new PDO($dsn, DATABASE_USERNAME, DATABASE_PASSWORD);
+        $this->connection = new PDO($dsn, DATABASE_USERNAME, DATABASE_PASSWORD);
+        $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+    }
 
-        $database_handle->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-
-        $statement = $database_handle->prepare($query);
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
         $statement->execute();
 
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
