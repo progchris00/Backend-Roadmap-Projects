40256a6d576034007a85edd0819a2ee57915ca12
progchris00 <prog.christian07@gmail.com>
2024-09-04 11:35:14 +0800

    feat: rename the database file name

---
+++ personal-blog/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+require "database.config.php";
+
+
+class Database
+{
+    public $connection;
+
+
+    public function __construct()
+    {
+        // Set the character set to UTF8. For encoding purposes.
+        $dsn = "mysql:host=localhost;dbname=test_db;charset=utf8mb4";
+
+
+        // Add an option to 
+
+        $this->connection = new PDO($dsn, DATABASE_USERNAME, DATABASE_PASSWORD, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+        $this->connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+    }
+
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+
+        return $statement;
+    }
+}
