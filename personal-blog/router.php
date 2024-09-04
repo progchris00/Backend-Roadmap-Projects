08264fb36d3296eb64f89f206e73a044d24bef8d
progchris00 <prog.christian07@gmail.com>
2024-09-04 09:25:32 +0800

    feat: modularize router.php contents and handle error pages

--- personal-blog/router.php
+++ personal-blog/router.php
@@ -1,6 +1,7 @@
 <?php
 
-$uri = $_SERVER["REQUEST_URI"];
+// This extracts the url only from the URI, disregarding other words.
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $routes = [
     "/" => "controllers/home.php",
@@ -8,6 +9,26 @@ $routes = [
     "/register" => "controllers/register.php",
 ];
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
+
+function routeToController($uri, $routes)
+{
+
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
 }
+
+// Return a meaningful http_response_code to the developer console and load the status code view.
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
+
+routeToController($uri, $routes);
