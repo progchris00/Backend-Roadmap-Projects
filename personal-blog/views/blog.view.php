ecfdb38e4d621b54786b0a7db1837d7ab421b579
progchris00 <prog.christian07@gmail.com>
2024-09-06 20:49:18 +0800

    feat: create a blog view and require the actual post

---
+++ personal-blog/views/blog.view.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$title = $blog_post["title"];
+
+require "views/partials/head.php";
+require "views/partials/nav.php";
+require "views/partials/blog-post.php";
+require "views/partials/foot.php";
