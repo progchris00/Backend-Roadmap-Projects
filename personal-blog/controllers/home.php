c6e174e7cc3433358b04a1b295125d57183c3635
progchris00 <prog.christian07@gmail.com>
2024-09-06 19:17:25 +0800

    feat: update sql query to join role

--- personal-blog/controllers/home.php
+++ personal-blog/controllers/home.php
@@ -4,6 +4,6 @@ $config = require "local.config.php";
 
 $db = new Database($config["database"]);
 
-$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.authorID=users.id WHERE blogs.isPublic=1;")->fetchAll();
+$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.authorID=users.id INNER JOIN role ON users.role=role.id WHERE blogs.isPublic=1;")->fetchAll();
 
 require "views/home.view.php";
