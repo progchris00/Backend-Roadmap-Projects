b47bc5f58916b27b8497d51a043b6e4af070d535
progchris00 <prog.christian07@gmail.com>
2024-09-06 19:17:07 +0800

    feat: make all information comming from database

--- personal-blog/views/partials/blog-contents.php
+++ personal-blog/views/partials/blog-contents.php
@@ -20,7 +20,7 @@
                         <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= $blog['body'] ?></p>
                     </div>
                     <div class="relative mt-8 flex items-center gap-x-4">
-                        <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                        <img src=<?= $blog["avatar"] ?> alt="" class="h-10 w-10 rounded-full bg-gray-50">
                         <div class="text-sm leading-6">
                             <p class="font-semibold text-gray-900">
                                 <a href="#">
@@ -28,7 +28,7 @@
                                     <?= ucfirst($blog['username']) ?>
                                 </a>
                             </p>
-                            <p class="text-gray-600">Co-Founder / CTO</p>
+                            <p class="text-gray-600"><?= ucfirst($blog["role"]) ?></p>
                         </div>
                     </div>
                 </article>
