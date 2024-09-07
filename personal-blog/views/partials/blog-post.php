67dc539d843269e71b381541c8c378afbc7e8c40
progchris00 <prog.christian07@gmail.com>
2024-09-07 16:52:21 +0800

    feat: style blog post partial

--- personal-blog/views/partials/blog-post.php
+++ personal-blog/views/partials/blog-post.php
@@ -1,5 +1,13 @@
 <div>
-    <p><?= $blog_post["category"] ?></p>
-    <h1><?= $blog_post["title"] ?></h1>
-    <p><?= $blog_post["body"] ?></p>
+    <div class="flex justify-center pt-10">
+        <div class="flex flex-col items-center w-1/2">
+            <div class="flex flex-col gap-y-2">
+                <a href="#" class="text-xs self-end z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $blog_post['category'] ?></a>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $blog_post["title"] ?></h1>
+                <p class="text-xs text-gray-500"><?= $blog_post["date"] ?></p>
+            </div>
+            <p class="mt-10"><?= $blog_post["body"] ?> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, placeat molestiae nulla fugiat quisquam saepe expedita magni atque nesciunt, velit illo! Possimus blanditiis commodi expedita ab ipsa. Ex corporis odit obcaecati perferendis voluptates illo corrupti ducimus, eum non. Earum, quia doloremque quibusdam sunt sequi dolorem pariatur mollitia excepturi numquam exercitationem eos ab, molestias amet suscipit similique totam asperiores velit aliquam cupiditate voluptatum incidunt facilis. Repudiandae quaerat quas iusto architecto rerum mollitia sit sint minima dolorem soluta asperiores nesciunt dignissimos sed, distinctio veritatis, accusamus exercitationem quam, blanditiis voluptas labore ut veniam! Laboriosam enim maxime aspernatur accusamus veritatis, labore blanditiis perspiciatis ratione consequuntur, voluptate omnis ullam reprehenderit molestiae quia quidem nisi incidunt, fuga modi amet dolores? Dicta ex, alias aliquid minima accusamus commodi! Officiis exercitationem delectus explicabo deleniti beatae asperiores assumenda quisquam in at, et, laboriosam, doloribus eligendi sit. Reiciendis illo facilis perferendis beatae. Quod nihil, nesciunt illo odit ducimus repellendus deserunt obcaecati ipsum porro voluptas magnam fugiat! Error doloribus tempore fuga voluptatibus, consectetur aliquid. Exercitationem reprehenderit dolores fuga nulla accusantium minima est officia? Sint debitis ipsum labore mollitia perferendis quia odit id et dolores alias eligendi officiis exercitationem minima nam numquam ad, quo, quas adipisci itaque porro ullam eaque. Natus, expedita!</p>
+        </div>
+    </div>
 </div>
