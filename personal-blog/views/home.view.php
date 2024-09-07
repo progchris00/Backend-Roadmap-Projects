bc253d731d336f2e082a5429ff2014aa69ab20ef
progchris00 <prog.christian07@gmail.com>
2024-09-07 16:53:13 +0800

    feat: add header partial on home.view

--- personal-blog/views/home.view.php
+++ personal-blog/views/home.view.php
@@ -5,5 +5,6 @@ $title = "Public blogs";
 
 require "views/partials/head.php";
 require "views/partials/nav.php";
+require "views/partials/header.php";
 require "views/partials/blog-contents.php";
 require "views/partials/foot.php";
