a4fd15d54fefe676f7bbc971ebeb3487c9e99fdb
progchris00 <prog.christian07@gmail.com>
2024-09-09 20:42:34 +0800

    feat: handle authorization for accessing specific blogs

--- personal-blog/controllers/blog.php
+++ personal-blog/controllers/blog.php
@@ -1,5 +1,7 @@
 <?php
 
+$isPublic = 1;
+
 $config = require "local.config.php";
 
 $db = new Database($config["database"]);
@@ -7,5 +9,14 @@ $db = new Database($config["database"]);
 $id = $_GET["id"];
 
 $blog_post = $db->query("SELECT * FROM blogs WHERE id = :id", ["id" => $id])->fetch();
+
+if (! $blog_post) {
+    abort();
+}
+
+if ($blog_post["isPublic"] != $isPublic) {
+    abort(Response::FORBIDDEN);
+}
+
 
 require "views/blog.view.php";
