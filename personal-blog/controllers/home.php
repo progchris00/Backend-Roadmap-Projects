64f133182b979be6edb08659f957fd65413f6c1d
progchris00 <prog.christian07@gmail.com>
2024-09-14 07:43:51 +0800

    feat: use findAll to query all blog posts

--- personal-blog/controllers/home.php
+++ personal-blog/controllers/home.php
@@ -4,6 +4,6 @@ $config = require "local.config.php";
 
 $db = new Database($config["database"]);
 
-$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.authorID=users.id INNER JOIN role ON users.role=role.id WHERE blogs.isPublic=1;")->fetchAll();
+$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.authorID=users.id INNER JOIN role ON users.role=role.id WHERE blogs.isPublic=1;")->findAll();
 
 require "views/home.view.php";
