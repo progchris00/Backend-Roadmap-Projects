10a2d402b60d46b491e5498fd44f6d778da81a6d
progchris00 <prog.christian07@gmail.com>
2024-09-13 12:15:54 +0800

    refractor: use the new method in querying blog

--- personal-blog/controllers/blog.php
+++ personal-blog/controllers/blog.php
@@ -8,11 +8,7 @@ $db = new Database($config["database"]);
 
 $id = $_GET["id"];
 
-$blog_post = $db->query("SELECT * FROM blogs WHERE id = :id", ["id" => $id])->fetch();
-
-if (! $blog_post) {
-    abort();
-}
+$blog_post = $db->query("SELECT * FROM blogs WHERE id = :id", ["id" => $id])->findOrFail();
 
 if ($blog_post["isPublic"] != $isPublic) {
     abort(Response::FORBIDDEN);
