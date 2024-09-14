81624c00b7c2b4a44ea2865281ce7108d8fdcf78
progchris00 <prog.christian07@gmail.com>
2024-09-14 11:05:57 +0800

    feat: handle form submit and authentication of user

--- personal-blog/controllers/login.php
+++ personal-blog/controllers/login.php
@@ -1,3 +1,23 @@
 <?php
 
 require "views/login.view.php";
+
+$config = require "local.config.php";
+
+$db = new Database($config["database"]);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $user = $db->query("SELECT * FROM users WHERE username = :username", ["username" => $username])->find();
+
+    if ($user["username"] == $username && $user["password"] == $password) {
+        $_SESSION["isLogin"] = true;
+        header("Location: /");
+    } else if ($user["username"] == $username && $user["password"] != $password) {
+        echo "Wrong password";
+    } else {
+        echo "User not found.";
+    }
+}
