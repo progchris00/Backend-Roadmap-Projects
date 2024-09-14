2a0935a898762dd3c2253d11584a23f705e5cd13
progchris00 <prog.christian07@gmail.com>
2024-09-14 17:39:26 +0800

    feat: implement adding a new blog to database

---
+++ personal-blog/controllers/create-blog.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = require "local.config.php";
+
+$db = new Database($config["database"]);
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+    $body = $_POST["body"];
+
+    $db->query(
+        "INSERT INTO blogs (title, body, isPublic, date, category, authorID, blogID) VALUES (:title, :body, :isPublic, :date, :category, :authorID, :blogID)",
+        ["title" => $title, "body" => $body, "isPublic" => 1, "date" => "2024-09-14", "category" => "Technology", "authorID" => $_SESSION["current_user"]["id"], "blogID" => 5]
+    );
+}
+
+require "views/create-blog.view.php";
