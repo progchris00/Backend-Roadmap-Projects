73d571283b58eae3f9fa34c83874801caa1d0207
progchris00 <prog.christian07@gmail.com>
2024-09-14 17:39:53 +0800

    feat: add temporary form for inputting blog

---
+++ personal-blog/views/partials/create-blog-form.php
@@ -0,0 +1,8 @@
+<form action="/create-blog" method="POST">
+    <label for="">Title</label>
+    <input type="text" name="title">
+
+    <label for="">Body</label>
+    <textarea name="body" id=""></textarea>
+    <button type="submit">Publish</button>
+</form>
