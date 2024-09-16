c7170bdee781fbfe34629fa61116eda03ed510c4
progchris00 <prog.christian07@gmail.com>
2024-09-16 20:35:12 +0800

    refractor: return the routes array

---
+++ personal-blog/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "/" => "controllers/home.php",
+    "/login" => "controllers/login.php",
+    "/register" => "controllers/register.php",
+    "/blog" => "controllers/blog.php",
+    "/logout" => "controllers/logout.php",
+    "/create-blog" => "controllers/create-blog.php",
+];
