be0c23e9a761a162ac0c69f197f6a9ea77dde59a
progchris00 <prog.christian07@gmail.com>
2024-09-18 16:55:27 +0800

    feat: escape contents from database before displaying to prevent malicious activity

--- personal-blog/views/partials/blog-contents.php
+++ personal-blog/views/partials/blog-contents.php
@@ -7,29 +7,29 @@
 
                 <article class="flex max-w-xl flex-col items-start justify-between">
                     <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500"><?= $blog['date'] ?></time>
+                        <time datetime="2020-03-16" class="text-gray-500"><?= htmlspecialchars($blog['date']) ?></time>
                         <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $blog['category'] ?></a>
                     </div>
                     <div class="group relative">
                         <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <a href="/blog?id=<?= $blog["blogID"] ?>">
+                            <a href="/blog?id=<?= htmlspecialchars($blog["blogID"]) ?>">
                                 <span class="absolute inset-0"></span>
-                                <?= $blog['title'] ?>
+                                <?= htmlspecialchars($blog['title']) ?>
                             </a>
                         </h3>
-                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= $blog['body'] ?></p>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= htmlspecialchars($blog['body']) ?></p>
                     </div>
                     <div class="relative mt-8 flex items-center gap-x-4">
-                        <img src=<?= $blog["avatar"] ?> alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                        <img src=<?= htmlspecialchars($blog["avatar"]) ?> alt="" class="h-10 w-10 rounded-full bg-gray-50">
                         <div class="text-sm leading-6">
                             <p class="font-semibold text-gray-900">
                                 <a href="#">
                                     <span class="absolute inset-0"></span>
                                     <?php
                                     if (isset($_SESSION["isLogin"])) {
-                                        echo $blog['username'] == $_SESSION["current_user"]["username"] ? "You" : ucfirst($blog["username"]);
+                                        echo $blog['username'] == $_SESSION["current_user"]["username"] ? "You" : htmlspecialchars(ucfirst($blog["username"]));
                                     } else {
-                                        echo ucfirst($blog["username"]);
+                                        echo htmlspecialchars(ucfirst($blog["username"]));
                                     } ?>
                                 </a>
                             </p>
