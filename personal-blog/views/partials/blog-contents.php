3adf3c21c483a38ac424d4ae8e77495678e40019
progchris00 <prog.christian07@gmail.com>
2024-09-20 20:07:58 +0800

    refractor: adjust blog arrays names

--- personal-blog/views/partials/blog-contents.php
+++ personal-blog/views/partials/blog-contents.php
@@ -7,29 +7,29 @@
 
                 <article class="flex max-w-xl flex-col items-start justify-between">
                     <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500"><?= htmlspecialchars($blog['date']) ?></time>
-                        <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $blog['category'] ?></a>
+                        <time datetime="2020-03-16" class="text-gray-500"><?= htmlspecialchars($blog['blog_creation_date']) ?></time>
+                        <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $blog['blog_category'] ?></a>
                     </div>
                     <div class="group relative">
                         <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <a href="/blog?id=<?= htmlspecialchars($blog["blogID"]) ?>">
+                            <a href="/blog?id=<?= htmlspecialchars($blog["blog_id"]) ?>">
                                 <span class="absolute inset-0"></span>
-                                <?= htmlspecialchars($blog['title']) ?>
+                                <?= htmlspecialchars($blog['blog_title']) ?>
                             </a>
                         </h3>
-                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= htmlspecialchars($blog['body']) ?></p>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= htmlspecialchars($blog['blog_body']) ?></p>
                     </div>
                     <div class="relative mt-8 flex items-center gap-x-4">
-                        <img src=<?= htmlspecialchars($blog["avatar"]) ?> alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                        <img src=<?= htmlspecialchars($blog["user_avatar"]) ?> alt="" class="h-10 w-10 rounded-full bg-gray-50">
                         <div class="text-sm leading-6">
                             <p class="font-semibold text-gray-900">
                                 <a href="#">
                                     <span class="absolute inset-0"></span>
                                     <?php
                                     if (isset($_SESSION["isLogin"])) {
-                                        echo $blog['username'] == $_SESSION["current_user"]["username"] ? "You" : htmlspecialchars(ucfirst($blog["username"]));
+                                        echo $blog['user_name'] == $_SESSION["current_user"]["username"] ? "You" : htmlspecialchars(ucfirst($blog["user_name"]));
                                     } else {
-                                        echo htmlspecialchars(ucfirst($blog["username"]));
+                                        echo htmlspecialchars(ucfirst($blog["user_name"]));
                                     } ?>
                                 </a>
                             </p>
