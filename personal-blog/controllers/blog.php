2cc088f0f5d0090e05e79933378980eea1b05c71
progchris00 <prog.christian07@gmail.com>
2024-09-20 20:06:53 +0800

    refractor: adjust column names to relate on the database column names

--- personal-blog/controllers/blog.php
+++ personal-blog/controllers/blog.php
@@ -8,8 +8,8 @@ $db = new Database($config["database"]);
 
 $id = $_GET["id"];
 
-$blog_post = $db->query("SELECT * FROM blogs WHERE id = :id", ["id" => $id])->findOrFail();
+$blog_post = $db->query("SELECT * FROM blogs WHERE blog_id = :id", ["id" => $id])->findOrFail();
 
-authorize($blog_post["isPublic"] == $isPublic);
+authorize($blog_post["blog_is_public"] == $isPublic);
 
 require "views/blog.view.php";
--- personal-blog/controllers/home.php
+++ personal-blog/controllers/home.php
@@ -4,6 +4,6 @@ $config = require "local.config.php";
 
 $db = new Database($config["database"]);
 
-$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.authorID=users.id INNER JOIN role ON users.role=role.id WHERE blogs.isPublic=1;")->findAll();
+$public_blogs = $db->query("SELECT * FROM blogs INNER JOIN users ON blogs.blog_author_id=users.user_id INNER JOIN roles ON users.user_role=roles.role_id WHERE blogs.blog_is_public=1;")->findAll();
 
 require "views/home.view.php";
